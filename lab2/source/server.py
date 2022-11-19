abcf2568064abc57c242a47edd008a1323705165
student <sprawka.k@gmail.com>
2022-11-19 12:10:33 +0100

    add exceptions handling

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -15,28 +15,25 @@ class web_server(http.server.SimpleHTTPRequestHandler):
 
         print(self.path)
 
-        # parsed_url = urlparse(self.path)
-        # captured_value = parse_qs(parsed_url.query)['cmd'][0]
-
-        # print(captured_value)
-        
-        # if self.path == '/':
         self.protocol_version = 'HTTP/1.1'
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=UTF-8")
         self.end_headers()            
-        self.wfile.write(b"Hello World!\n")
+        
 
-
-        cmd = parse.parse_qs(parse.urlparse(self.path).query)['cmd'][0]
+        try:
+            cmd = parse.parse_qs(parse.urlparse(self.path).query)['cmd'][0]
         # self.wfile.write(str.encode(param))
-        if cmd == 'time':
-            time = datetime.now().strftime("%H:%M:%S")
-            self.wfile.write(str.encode(time))
-        elif cmd == 'rev':
-            str_to_rev = parse.parse_qs(parse.urlparse(self.path).query)['str'][0]
-            reversed_str = str_to_rev[::-1]
-            self.wfile.write(str.encode(reversed_str))
+            if cmd == 'time':
+                time = datetime.now().strftime("%H:%M:%S")
+                self.wfile.write(str.encode(time))
+            elif cmd == 'rev':
+                str_to_rev = parse.parse_qs(parse.urlparse(self.path).query)['str'][0]
+                reversed_str = str_to_rev[::-1]
+                self.wfile.write(str.encode(reversed_str))
+        except:
+            self.wfile.write(b"Hello World!\n")
+
 
 
 
