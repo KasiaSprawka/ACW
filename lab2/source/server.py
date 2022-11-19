87ce782dc730aac3d62481596f7dc2f4a02af2dd
student <sprawka.k@gmail.com>
2022-11-19 11:57:06 +0100

    add query params handling

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -3,7 +3,9 @@ import http.server
 import socketserver
 import os
 from datetime import datetime
-import urlparse
+# from urllib.parse import urlparse
+# from urllib.parse import parse_qs
+from urllib import parse
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -12,21 +14,34 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
-        parsed = urlparse.urlparse(self.path)
 
-        captured_value = urlparse.parse_qs(parsed.query)['cmd'][0]
-        print captured_value
+        # parsed_url = urlparse(self.path)
+        # captured_value = parse_qs(parsed_url.query)['cmd'][0]
+
+        # print(captured_value)
         
-        if self.path == '/':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+        # if self.path == '/':
+        self.protocol_version = 'HTTP/1.1'
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=UTF-8")
+        self.end_headers()            
+        self.wfile.write(b"Hello World!\n")
+
+
+        cmd = parse.parse_qs(parse.urlparse(self.path).query)['cmd'][0]
+        # self.wfile.write(str.encode(param))
+        if cmd == 'time':
             time = datetime.now().strftime("%H:%M:%S")
             self.wfile.write(str.encode(time))
-        else:
-            super().do_GET()
+        elif cmd == 'rev':
+            str_to_rev = parse.parse_qs(parse.urlparse(self.path).query)['str'][0]
+            reversed_str = str_to_rev[::-1]
+            self.wfile.write(str.encode(reversed_str))
+
+
+
+        # else:
+        #     super().do_GET()
     
 # --- main ---
 
